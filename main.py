a4f17eae959a5a22377a18f5289e0169da432ba0
FaisalAlhatimi <alhatimifaisaldev@gmail.com>
2026-09-02 00:01:37 +0300

    add: user input

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI, responses
-
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 if api_key == None:
     raise RuntimeError("api key is not setup")
+
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 
 client = OpenAI(
@@ -20,7 +25,7 @@ response = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            "content": f"{args.user_prompt}",
         }
     ],
 )
