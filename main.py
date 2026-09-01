502ce8d4b54bfe6e8ceb577b490f8d48204aec78
FaisalAlhatimi <alhatimifaisaldev@gmail.com>
2026-09-01 20:22:12 +0300

    add: setup openrouter api

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key == None:
+    raise RuntimeError("api key is not setup")
+
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+
 def main():
-    print("Hello from aiagent!")
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
